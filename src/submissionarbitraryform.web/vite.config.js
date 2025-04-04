b8378d221728792b6abf70f09136880f61a97fbc
Sergey <code4builder@gmail.com>
2025-04-04 14:24:22 +0200

    Bundle using Vite

--- src/submissionarbitraryform.web/vite.config.js
+++ src/submissionarbitraryform.web/vite.config.js
@@ -3,17 +3,18 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+// To fix __dirname and __filename in ES Modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-    plugins: [vue()],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'src'),
-        },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),  // Create an alias for the 'src' directory
     },
-    server: {
-        port: 50680,
-    }
+  },
+  server: {
+    port: 50680,  // Define the port for the dev server
+  },
 });
